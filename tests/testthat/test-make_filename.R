5d0d8f3cc558c420c80bb0c5029b7bf746d95f3b
Williams Ojo <wllmsojo@gmail.com>
2019-06-01 19:10:10 +0100

    changing the test file

--- tests/testthat/test-make_filename.R
+++
@@ -1,5 +0,0 @@
-test_that("File name constructs normally", {
-  expect_identical(make_filename(2013), "accident_2013.csv.bz2")
-  expect_identical(make_filename(2020), "accident_2020.csv.bz2")
-  expect_identical(make_filename("ab"), "accident_NA.csv.bz2")
-})
